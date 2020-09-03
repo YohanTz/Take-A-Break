d345b1e554835a2c8c5c134b67418a0a251789bf
Yohan Tancrez <yohan.tancrez@epita.fr>
2020-09-03 20:05:45 +0200

    Initial Commit

---
+++ Take A Break/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Take A Break
+//
+//  Created by Yohan Tancrez on 03/09/2020.
+//  Copyright © 2020 Yohan Tancrez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Take A Break/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Take A Break
+//
+//  Created by Yohan Tancrez on 03/09/2020.
+//  Copyright © 2020 Yohan Tancrez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
